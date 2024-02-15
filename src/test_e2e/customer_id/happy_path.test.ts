b2e389c1c4e12f97b628e0cf7e71a2f744be8ff7
Tiwthawat Somaumchan <liew8444@gmail.com>
2024-02-15 15:55:20 +0700

    feat: :green_heart: happy_up2

---
+++ src/test_e2e/customer_id/happy_path.test.ts
@@ -0,0 +1,19 @@
+import request from "supertest";
+import { app, pool } from "../../app";
+afterEach(() => {
+	pool.end();
+});
+
+test("GET customers by iD", async () => {
+	const res = await request(app).get("/customers/1").expect("Content-Type", /json/).expect(200);
+
+	const response = await request(app).get("/customers/1");
+
+	expect(response.status).toBe(200);
+
+	const data: Array<{ id: number }> = response.body.result;
+
+	expect(data.every(({ id }) => id === 1)).toBe(true);
+
+	expect(data.length === 1).toBe(true);
+});
